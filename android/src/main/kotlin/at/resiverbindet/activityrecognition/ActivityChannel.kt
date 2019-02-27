240908cbfebc08f88155aeef4f7cef14d642d5e2
David <david@knight.fm>
2019-02-27 12:06:26 -0800

    up kotlin is driving me crazy 5

--- android/src/main/kotlin/at/resiverbindet/activityrecognition/ActivityChannel.kt
+++ android/src/main/kotlin/at/resiverbindet/activityrecognition/ActivityChannel.kt
@@ -10,8 +10,8 @@ import at.resiverbindet.activityrecognition.activity.ActivityClient
 import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
-import kotlinx.coroutines.experimental.android.UI
-import kotlinx.coroutines.experimental.launch
+// import kotlinx.coroutines.android.ui
+import kotlinx.coroutines.launch
 
 class ActivityChannel(private val activityClient: ActivityClient) :
         MethodChannel.MethodCallHandler, EventChannel.StreamHandler {
